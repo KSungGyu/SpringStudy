154b4f66eb0a4c2f6d905454c5bfc8cf93a00bba
elwn1 <ksg980105@naver.com>
2022-10-17 20:57:00 +0900

    중복확인

---
+++ SpringMVC03/src/main/java/kr/board/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package kr.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import kr.board.entity.Board;
+import kr.board.entity.Member;
+
+@Mapper //- Mybatis API
+public interface MemberMapper {	
+	public Member registerCheck(String memID);
+     
+}
