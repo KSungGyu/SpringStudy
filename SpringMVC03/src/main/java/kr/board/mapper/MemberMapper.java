393cdde83d8e1866579fd7391c99c1c5d865570c
elwn1 <ksg980105@naver.com>
2022-10-26 18:11:27 +0900

    수정처리+navbar수정

--- SpringMVC03/src/main/java/kr/board/mapper/MemberMapper.java
+++ SpringMVC03/src/main/java/kr/board/mapper/MemberMapper.java
@@ -14,4 +14,5 @@ public interface MemberMapper {
 	public Member registerCheck(String memID);
     public int register(Member m); //회원등록( 1, 0)
     public Member memLogin(Member mvo); // 로그인체크
+    public int memUpdate(Member mvo); // 수정하기
 }
